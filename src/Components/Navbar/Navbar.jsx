f18f224db3baefbe24fef7f16e4eca8fe159d146
Christian Endale <christiangirmachew@gmail.com>
2025-10-23 21:26:04 +0100

    `Updated AnchorLink to Link from react-scroll in Navbar component`

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import logo from "../../assets/myLogo.png";
 import underline from "../../assets/nav_underline.svg";
-import AnchorLink from "react-anchor-link-smooth-scroll";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
 	const [menu, setMenu] = useState("home");
@@ -11,29 +11,61 @@ const Navbar = () => {
 			<img src={logo} width={"140px"} alt="" />
 			<ul className="navMenu">
 				<li>
-					<AnchorLink className="anchorLink" href="#home">
-						<p onClick={() => setMenu("home")}>Home</p>
-					</AnchorLink>
+					<Link
+						activeClass="active" // Class to apply when link is active (optional)
+						to="home" // The ID of the section to scroll to
+						spy={true} // Makes the link "active" when you scroll to it
+						smooth={true} // Enables smooth scrolling
+						offset={-70} // Your 50px offset (adjust as needed)
+						duration={500} // Animation speed
+						onSetActive={() => setMenu("home")} // Sets menu on scroll
+					>
+						Home
+					</Link>
 					{menu === "home" ? <img src={underline} /> : <></>}
 				</li>
 				<li>
-					<AnchorLink className="anchorLink" offset={50} href="#about">
-						<p onClick={() => setMenu("about")}>About Me</p>
-					</AnchorLink>
+					<Link
+						activeClass="active"
+						to="about"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						onSetActive={() => setMenu("about")}
+					>
+						About Me
+					</Link>
 					{menu === "about" ? <img src={underline} /> : <></>}
 				</li>
 				<li>
-					<AnchorLink className="anchorLink" offset={50} href="#projects">
-						<p onClick={() => setMenu("projects")}>Projects</p>
-					</AnchorLink>
+					<Link
+						activeClass="active"
+						to="projects"
+						spy={true}
+						smooth={true}
+						offset={-70}
+						duration={500}
+						onSetActive={() => setMenu("projects")}
+					>
+						Projects
+					</Link>
 					{menu === "projects" ? <img src={underline} /> : <></>}
 				</li>
 			</ul>
 
 			<div className="navConnect">
-				<AnchorLink className="anchorLink" offset={50} href="#contact">
-					<p onClick={() => setMenu("contact")}>Contact Me</p>
-				</AnchorLink>
+				<Link
+					activeClass="active"
+					to="contact"
+					spy={true}
+					smooth={true}
+					offset={-70}
+					duration={500}
+					onSetActive={() => setMenu("contact")}
+				>
+					Contact Me
+				</Link>
 			</div>
 		</div>
 	);
