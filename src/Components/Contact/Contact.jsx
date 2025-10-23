945aa87c53a8bbed3c4bc7cffbe580692f6b9985
Christian Endale <christiangirmachew@gmail.com>
2025-10-23 21:40:20 +0100

    `Added state management and form submission functionality to Contact component, updated Navbar links to use Link component with activeClass and onSetActive props.`

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 import themePattern from "../../assets/theme_pattern.svg";
 import mailIcon from "../../assets/mail_icon.svg";
@@ -6,6 +6,28 @@ import phoneIcon from "../../assets/call_icon.svg";
 import locationIcon from "../../assets/location_icon.svg";
 
 const Contact = () => {
+	const [result, setResult] = useState("");
+
+	const onSubmit = async (event) => {
+		event.preventDefault();
+		setResult("Sending....");
+		const formData = new FormData(event.target);
+		formData.append("access_key", "3a53a50a-d425-4269-a92e-ab14858f05f2");
+
+		const response = await fetch("https://api.web3forms.com/submit", {
+			method: "POST",
+			body: formData,
+		});
+
+		const data = await response.json();
+		if (data.success) {
+			alert("Form Submitted Successfully");
+			event.target.reset();
+		} else {
+			setResult("Error");
+		}
+	};
+
 	return (
 		<div>
 			<div id="contact" className="contact">
@@ -17,10 +39,7 @@ const Contact = () => {
 				<div className="contactSection">
 					<div className="contactLeft">
 						<h1>Talk to me</h1>
-						<p>
-							I am currently open to new opportunities. Whether you have a question or
-							just want to say hi, feel free to reach out!
-						</p>
+						<p>I am currently open to new opportunities. Whether you have a question or just want to say hi, feel free to reach out!</p>
 						<div className="contactDetails">
 							<div className="contactDetail">
 								<img src={mailIcon} alt="" /> <p>christiangirmachew@gmail.com</p>
@@ -33,7 +52,7 @@ const Contact = () => {
 							</div>
 						</div>
 					</div>
-					<form action="" className="contactRight">
+					<form onSubmit={onSubmit} className="contactRight">
 						<label htmlFor="">Name</label>
 						<input type="text" placeholder="Enter Your Name" name="name" />
 						<label htmlFor="">Email</label>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -11,43 +11,19 @@ const Navbar = () => {
 			<img src={logo} width={"140px"} alt="" />
 			<ul className="navMenu">
 				<li>
-					<Link
-						activeClass="active" // Class to apply when link is active (optional)
-						to="home" // The ID of the section to scroll to
-						spy={true} // Makes the link "active" when you scroll to it
-						smooth={true} // Enables smooth scrolling
-						offset={-70} // Your 50px offset (adjust as needed)
-						duration={500} // Animation speed
-						onSetActive={() => setMenu("home")} // Sets menu on scroll
-					>
+					<Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500} onSetActive={() => setMenu("home")}>
 						Home
 					</Link>
 					{menu === "home" ? <img src={underline} /> : <></>}
 				</li>
 				<li>
-					<Link
-						activeClass="active"
-						to="about"
-						spy={true}
-						smooth={true}
-						offset={-70}
-						duration={500}
-						onSetActive={() => setMenu("about")}
-					>
+					<Link activeClass="active" to="about" spy={true} smooth={true} offset={50} duration={500} onSetActive={() => setMenu("about")}>
 						About Me
 					</Link>
 					{menu === "about" ? <img src={underline} /> : <></>}
 				</li>
 				<li>
-					<Link
-						activeClass="active"
-						to="projects"
-						spy={true}
-						smooth={true}
-						offset={-70}
-						duration={500}
-						onSetActive={() => setMenu("projects")}
-					>
+					<Link activeClass="active" to="projects" spy={true} smooth={true} offset={50} duration={500} onSetActive={() => setMenu("projects")}>
 						Projects
 					</Link>
 					{menu === "projects" ? <img src={underline} /> : <></>}
@@ -55,15 +31,7 @@ const Navbar = () => {
 			</ul>
 
 			<div className="navConnect">
-				<Link
-					activeClass="active"
-					to="contact"
-					spy={true}
-					smooth={true}
-					offset={-70}
-					duration={500}
-					onSetActive={() => setMenu("contact")}
-				>
+				<Link activeClass="active" to="contact" spy={true} smooth={true} offset={-70} duration={500} onSetActive={() => setMenu("contact")}>
 					Contact Me
 				</Link>
 			</div>
