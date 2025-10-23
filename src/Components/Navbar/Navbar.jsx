6b5dfb42af771970f2dd74501cb7bfd14b5c16cc
Christian Endale <christiangirmachew@gmail.com>
2025-10-23 21:57:16 +0100

    `Added responsive navbar functionality with mobile menu and close button`

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./Navbar.css";
 import logo from "../../assets/myLogo.png";
 import underline from "../../assets/nav_underline.svg";
 import { Link } from "react-scroll";
+import menuOpen from "../../assets/menu_open.svg";
+import menuClose from "../../assets/menu_close.svg";
 
 const Navbar = () => {
 	const [menu, setMenu] = useState("home");
+	const menuRef = useRef();
+
+	const openMenu = () => {
+		menuRef.current.style.right = "0";
+	};
+
+	const closeMenu = () => {
+		menuRef.current.style.right = "-400px";
+	};
 	return (
 		<div className="navbar">
 			<img src={logo} width={"140px"} alt="" />
-			<ul className="navMenu">
+			<img src={menuOpen} onClick={openMenu} alt="" className="openNav" />
+			<ul ref={menuRef} className="navMenu">
+				<img src={menuClose} onClick={closeMenu} alt="" className="closeNav" />
 				<li>
 					<Link activeClass="active" to="home" spy={true} smooth={true} offset={50} duration={500} onSetActive={() => setMenu("home")}>
 						Home
